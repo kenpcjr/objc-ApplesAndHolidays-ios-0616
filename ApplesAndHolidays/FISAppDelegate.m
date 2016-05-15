595f59664fa060abaf5ecc9858b40f013078b8ac
kenpcjr <kennethcooke@me.com>
2016-05-15 17:08:59 -0400

    Twelve Pass

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,10 +11,50 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+-(NSArray *)pickApplesFromFruits:(NSArray *)fruits{
+    NSPredicate *predOnlyApples = [NSPredicate predicateWithFormat:@"self contains 'apple'"];
+    NSArray *onlyApples = [fruits filteredArrayUsingPredicate:predOnlyApples];
+    return onlyApples;
+}
+
+-(NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSArray *holidays = [database[season]allKeys];
+    /*NSPredicate *predHolidays = [NSPredicate predicateWithFormat:@"self contains 'Day'"];
+    NSArray *filteredHolidays = [holidays filteredArrayUsingPredicate:predHolidays];
+    
+    
+    
+    NSArray *databaseKeys = [database allKeys];
+    NSLog(@"%@",databaseKeys);
+    NSArray *holidays = [databaseKeys filteredArrayUsingPredicate:predHolidays];*/
+    NSLog(@"%@",holidays);
+    return holidays;
+}
+
+-(NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    NSMutableArray *supplies = database[season][holiday];
+    return supplies;
+}
+
+-(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    BOOL holidayIndicator = NO;
+    NSArray *seasonKeys = [database[season]allKeys];
+    if ([seasonKeys containsObject:holiday]) {
+        holidayIndicator = YES;
+    }
+    return holidayIndicator;
+}
+
+-(BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return nil;
+}
+
+-(NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return nil;
+}
+
+-(NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database {
+    return nil;
+}
 
 @end
